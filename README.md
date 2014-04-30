fa68ea7f3fd9fda2f73cdf4a4309c0d3f88cd783
rosie <budgetaus@hotmail.com>
2014-04-30 15:57:19 +1000

    adding GNU General Public Licence

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 The working version of BudgetAus is online at http://infoaus.net/budget/index.php
 
-The public working version of the code at this address may not match the code in the repository as changes have not been pushed to the server.
-Please note that for 'last' year's budget figures (figures published on budget night 2012) there are only figures for 2011-12 and 2012-13 and no forward funding years.
+The public working version of the code at this address may not match the code in the repository as pushing to Git does not push to the server.
 
-Until further notice code is copyright to Rosie Williams.
+Please note that for 'last' year's budget figures (figures published on budget night 2012) there are only figures for 2011-12 and 2012-13 and no forward funding years. 
+
+This code is licenced under the GNU General Public Licence https://www.gnu.org/licenses/gpl.html
--- estimated_v_actual.php
+++ estimated_v_actual.php
@@ -47,64 +47,6 @@ $agencies = mysql_query("SELECT AGENCY from budget_table GROUP BY AGENCY ");//cr
 
 
 
-<?php
-include('scripts/db.php');
-
-if (mysql_select_db($db_database))
-{
-$agency = $_GET['agency']; 
-}
-{
-$budget_year = $_GET['budget_year']; 
-}
-$agency=mysql_real_escape_string($agency);
-if(isset($_GET['agency']))
-{
-
-
- 
- $result_last_non_boolean =  mysql_query("SELECT Agency,Acronym,last,sum(last),current,sum(current),source,url
-FROM budget_table  WHERE (Agency) LIKE('%$agency%') GROUP BY Agency ");
-// this query asks for the total for the current year from the LAST BUDGET YEAR DATA for the agencies matching the user search term. This value for the last budget year data will be the ESTIMATED cost. This search is NON BOOLEAN.
-      
-
- $num_rows = mysql_num_rows($result_last_non_boolean);
- ($rows = mysql_num_rows($result_last_non_boolean));
-     
-          for ($j = 0 ; $j < $rows ; ++$j)
-	 
-$result_current_non_boolean =  mysql_query("SELECT Portfolio,Program,Agency,Acronym,last,sum(last),current,sum(current),source,url 
-FROM budget_table2  WHERE (Agency) LIKE('%$agency%')  &&  NOT('%Workplace Gender Equality Agency%') GROUP BY Agency ");
-//this query asks for the total for the agencies matching the user input search term from the CURRENT BUDGET YEAR DATA for the column relating to 'last' year. This is going to be the ACTUAL spend as these are the REVISED figures for what was ESTIMATED in last year's budget data. With the value from this query and the one above, these two values can be compared and a difference calculated between ESTIMATED and ACTUAL spend for agencies that match the search term. This search is NON BOOLEAN.
-       
-
- $num_rows = mysql_num_rows($result_current_non_boolean);
-
-   ($rows = mysql_num_rows($result_current_non_boolean));
-     
-          for ($j = 0 ; $j < $rows ; ++$j)
-		  
-          echo
-"<table class='results'>
-<tr><td class='left'>Agency</td>
-<td><a href='agency_results.php?agency=%22".mysql_result($result_current_non_boolean,$j, 'Agency')."%22&budget_year=current'   title='Find all Agency results for ".mysql_result($result_current_non_boolean,$j, 'Agency')." 'target='_blank' '>".mysql_result($result_current_non_boolean,$j, 'Agency')."</a> (".mysql_result($result_current_non_boolean,$j, 'ACRONYM').")
-</td></tr>
-<TR>
-<td>Estimated<TD class='money'><a href=" .mysql_result($result_last_non_boolean,$j, 'URL').' target="_blank" title="Opens in new window">' .mysql_result($result_last_non_boolean,$j, 'Source')."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-$".number_format(mysql_result($result_last_non_boolean, $j, 'sum(current)')).",000  </TD>
-</tr>
-<td>Actual<TD class='money'><a href=" .mysql_result($result_current_non_boolean,$j, 'URL').' target="_blank" title="Opens in new window">' .mysql_result($result_current_non_boolean,$j, 'Source')."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-$".number_format(mysql_result($result_current_non_boolean, $j, 'sum(last)')).",000</TD>
-</tr>
-<tr>
-<td>Difference</td><td class='money'>$".number_format(mysql_result($result_current_non_boolean, $j, 'sum(last)') - mysql_result($result_last_non_boolean, $j, 'sum(current)')).",000</td>
-</tr>
-</table>";
-//////////////////////////////////////////////////////////////////////////////////////  
-
-	}
-		
-?>
 
 <?php
 include('scripts/db.php');
@@ -114,49 +56,36 @@ if (mysql_select_db($db_database))
 $agency = $_GET['agency']; 
 }
 
-if(!isset($_GET['agency']))
-{
 
-$all_agencies = mysql_query("Select distinct BI.Agency, BA.B2Last, BA.B2Current, BB.B1last, BB.B1Current from
-(Select distinct BU.Agency from
-((select B2.Agency
-from budget_table2 as B2
-group by Agency) union distinct (
-select B1.Agency
-from budget_table1 as B1
-group by B1.Agency) 
-) as BU) BI left join 
-(select B2.Agency, sum(B2.last) B2last, sum(B2.current) B2Current
-from budget_table2 as B2
-group by Agency) BA on BI.Agency = BA.Agency
-left join 
-(select B1.Agency, sum(B1.last) B1Last, sum(B1.current) B1Current
-from budget_table1 B1
-group by B1.Agency) BB on BI.Agency = BB.Agency");
 
- $num_rows = mysql_num_rows($all_agencies);
+$row = mysql_query("SELECT * from agencies");
 
-   ($rows = mysql_num_rows($all_agencies));
+ $num_rows = mysql_num_rows($row);
+
+   ($rows = mysql_num_rows($row));
      
           for ($j = 0 ; $j < $rows ; ++$j)
+		  
+{
 		  
           echo
 "<table class='results'>
 <tr><td class='left'>Agency</td>
-<td><a href='agency_results.php?agency=%22".mysql_result($all_agencies,$j, 'Agency')."%22&budget_year=current'   title='Find all Agency results for ".mysql_result($all_agencies,$j, 'Agency')." 'target='_blank' '>".mysql_result($all_agencies,$j, 'Agency')."</a> (".mysql_result($all_agencies,$j, 'ACRONYM').")
+<td><a href='agency_results.php?agency=%22".mysql_result($row,$j, 'Agency')."%22&budget_year=current'   title='Find all Agency results for ".mysql_result($row,$j, 'Agency')." 'target='_blank' '>".mysql_result($row,$j, 'Agency')."</a> 
 </td></tr>
 <TR>
-<td>Estimated<TD class='money'><a href=" .mysql_result($all_agencies,$j, 'URL').' target="_blank" title="Opens in new window">' .mysql_result($all_agencies,$j, 'Source')."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-$".number_format(mysql_result($all_agencies, $j, 'sum(B1current)')).",000  </TD>
+<td>Estimated<TD class='money'>
+$".number_format(mysql_result($row, $j, 'Budget_year_one_current')).",000  </TD>
 </tr>
-<td>Actual<TD class='money'><a href=" .mysql_result($all_agencies,$j, 'URL').' target="_blank" title="Opens in new window">' .mysql_result($all_agencies,$j, 'Source')."</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-$".number_format(mysql_result($all_agencies, $j, 'sum(B2last)')).",000</TD>
+<td>Actual<TD class='money'>
+$".number_format(mysql_result($row, $j, 'Budget_year_two_last')).",000</TD>
 </tr>
 <tr>
-<td>Difference</td><td class='money'>$".number_format(mysql_result($all_agencies, $j, 'sum(B2last)') - mysql_result($all_agencies, $j, 'sum(B1current)')).",000</td>
+<td>Difference</td><td class='money'>$".number_format(mysql_result($row, $j, 'Budget_year_two_last') - mysql_result($row, $j, 'Budget_year_one_current')).",000</td>
 </tr>
 </table>";
 }
+
 ?>
 
 </div>
